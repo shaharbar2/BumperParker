bf03f1c2322082c30e27f62b5089ff56bf927612
barshp <>
2020-03-11 01:17:19 +0200

    * Organised code * Tweeked some movement numbers * Removed old comments

--- Assets/Scripts/CarConrollerv2.cs
+++ Assets/Scripts/CarConrollerv2.cs
@@ -6,6 +6,7 @@ public class CarConrollerv2 : MonoBehaviour
 {
     private Rigidbody rb;
     private float steeringAngle;
+    private float forwardVelocity;
     private float horizontalInput;
     private float verticalInput;
 
@@ -17,38 +18,20 @@ public class CarConrollerv2 : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        // targetRotation = Quaternion.identity;
     }
 
     private void FixedUpdate()
     {
         GetInput();
+        steeringAngle = maxSteerAngle * horizontalInput;
+        forwardVelocity = transform.InverseTransformDirection(rb.velocity).z;
         if (IsGrounded())
         {
-            // Steer();
-            // Accelerate();
-
-            var forwardVelocity = transform.InverseTransformDirection(rb.velocity).z;
-
-            // Steer
-            steeringAngle = maxSteerAngle * horizontalInput;
-            var rotateMultiplier = Mathf.Clamp(forwardVelocity * 0.1f, -1, 1);
-            rb.rotation *= Quaternion.Euler(Vector3.up * steeringAngle * rotationSpeed * rotateMultiplier * Time.deltaTime);
-
-            // Move Forward
-            rb.AddRelativeForce(Vector3.forward * verticalInput * acceleration * Time.deltaTime, ForceMode.VelocityChange);
-
-            foreach (Transform wheel in wheels)
-            {
-                if (wheel.tag == "FrontWheel")
-                {
-                    wheel.localEulerAngles = Vector3.up * steeringAngle;
-                }
-                wheel.GetChild(0).Rotate(Vector3.right * forwardVelocity * 2, Space.Self);
-            }
+            Steer();
+            Accelerate();
         }
 
-        Brake();
+        // TODO: Implement Brake();
         UpdateWheelPoses();
     }
 
@@ -58,52 +41,15 @@ public class CarConrollerv2 : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
     }
 
-    private bool IsGrounded()
-    {
-        Debug.Log("Is Grounded = " + Physics.Raycast(rb.position, -rb.transform.up, 0.5f));
-        return Physics.Raycast(rb.position, -rb.transform.up, 1f);
-    }
-
     private void Steer()
     {
-        // //transform.InverseTransformDirection(rb.velocity).normalized
-        // //Vector3.Dot(rb.velocity, rb.transform.forward)
-        // // var velocity = rb.velocity.normalized.x + rb.velocity.normalized.z;
-        // // var velocity = Mathf.Abs(rb.velocity.normalized.x) + Mathf.Abs(rb.velocity.normalized.z);
-        // var velocity = Mathf.Abs(Vector3.Dot(rb.velocity, Vector3.forward));
-        // Debug.Log("Velocity: " + velocity);
-        // // Debug.Log(Vector3.Dot(rb.velocity, rb.transform.forward));
-
-        // // if (velocity != 0)
-        // // {
-        // steeringAngle = maxSteerAngle * horizontalInput;
-        // // Quaternion deltaRotation = Quaternion.Euler(Vector3.up * steeringAngle);
-        // // rb.MoveRotation(rb.rotation * deltaRotation);
-
-        // // if (velocity > 0.0001f)
-        // // {
-        // // verticalInput is added to lower the rotation when slowing down
-        // // Quaternion deltaRotation = Quaternion.Euler(Vector3.up * steeringAngle * steeringSpeed * Time.deltaTime);
-        // // rb.rotation = Quaternion.RotateTowards(rb.rotation, rb.rotation * deltaRotation, maxSteerAngle);
-
-        // Quaternion deltaRotation = Quaternion.Euler(Vector3.up * steeringAngle * steeringSpeed * Time.deltaTime);
-        // // rb.rotation = Quaternion.RotateTowards(rb.rotation, rb.rotation * deltaRotation, Mathf.Lerp(maxSteerAngle * velocity, maxSteerAngle, velocity));
-        // // Debug.Log("Yeet " + Mathf.Lerp(velocity, 100, velocity) * 0.01f);
-        // // rb.rotation = Quaternion.Lerp(transform.rotation, rb.rotation * deltaRotation, Mathf.Lerp(0, maxSteerAngle, Mathf.Lerp(0, 1, velocity * Time.deltaTime)));
-
-        // var steeringLerpT = velocity / maxSteerAngle;
-        // var rotationLerp = Mathf.Lerp(0, maxSteerAngle, steeringLerpT);
-        // Debug.Log("steeringLerpT " + steeringLerpT);
-        // Debug.Log("rotationLerp " + rotationLerp);
-        // rb.rotation = Quaternion.Lerp(transform.rotation, rb.rotation * deltaRotation, rotationLerp);
+        var rotateMultiplier = Mathf.Clamp(forwardVelocity * 0.1f, -1, 1);
+        rb.rotation *= Quaternion.Euler(Vector3.up * steeringAngle * rotationSpeed * rotateMultiplier * Time.deltaTime);
     }
 
     private void Accelerate()
     {
-        // if (IsGrounded())
-        // {
-        //     rb.AddRelativeForce(Vector3.forward * verticalInput * motorForce * Time.deltaTime, ForceMode.VelocityChange);
-        // }
+        rb.AddRelativeForce(Vector3.forward * verticalInput * acceleration * Time.deltaTime, ForceMode.VelocityChange);
     }
 
     private void Brake()
@@ -113,15 +59,20 @@ public class CarConrollerv2 : MonoBehaviour
 
     private void UpdateWheelPoses()
     {
-        var wheels = GameObject.FindGameObjectsWithTag("FrontWheel");
-        foreach (var wheel in wheels)
+        foreach (Transform wheel in wheels)
         {
-            UpdateWheelPose(wheel.transform);
+            if (wheel.tag == "FrontWheel")
+            {
+                wheel.localEulerAngles = Vector3.up * steeringAngle;
+            }
+            // TODO: Allow wheel rotation with velocity even if not moving (upside down)
+            wheel.GetChild(0).Rotate(Vector3.right * forwardVelocity * 2, Space.Self);
         }
     }
 
-    private void UpdateWheelPose(Transform wheel)
+    private bool IsGrounded()
     {
-        // wheel.localRotation = Quaternion.AngleAxis(steeringAngle, Vector3.up);
+        Debug.Log("Is Grounded = " + Physics.Raycast(rb.position, -rb.transform.up, 0.5f));
+        return Physics.Raycast(rb.position, -rb.transform.up, 1f);
     }
 }
