fe90fe25364b8da403f1f5e55963735147bd1a45
barshp <>
2020-04-06 19:12:18 +0300

    Improved the parking spawner system (lowers minimum distance on retries limit reached)

--- Assets/Scripts/ParkingSpawnerController.cs
+++ Assets/Scripts/ParkingSpawnerController.cs
@@ -9,13 +9,14 @@ public class ParkingSpawnerController : MonoBehaviour
     [SerializeField] private int maxParkings = 4;
 
     [SerializeField] private Vector3 size;
-    [SerializeField] private float minimumDistanceFromPlayer;
-    [SerializeField] private float minimumDistanceFromParking;
+    [SerializeField] private float _minimumDistanceFromPlayer;
+    [SerializeField] private float _minimumDistanceFromParking;
     [SerializeField] private Color GizmosColor = new Color(1, 0, 0, 0.2f);
 
     private List<GameObject> currentPlayers;
     // TODO: Change parkings to object pooling
     private List<GameObject> currentParkings;
+    private int spawnRetries = 15;
 
     void Start()
     {
@@ -26,8 +27,6 @@ public class ParkingSpawnerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Players: " + currentPlayers.Count);
-        Debug.Log("Parkings: " + currentParkings.Count);
         if (Input.GetKeyDown(KeyCode.Q))
         {
             SpawnParking();
@@ -40,8 +39,6 @@ public class ParkingSpawnerController : MonoBehaviour
         Gizmos.DrawCube(transform.localPosition, size);
     }
 
-
-
     private Vector3 GetRandomPosition()
     {
         return transform.position +
@@ -50,7 +47,7 @@ public class ParkingSpawnerController : MonoBehaviour
                             0,
                             Random.Range(-size.z / 2, size.z / 2));
     }
-    private bool IsPositionInvalid(Vector3 pos)
+    private bool IsPositionInvalid(Vector3 pos, float minimumDistanceFromPlayer, float minimumDistanceFromParking)
     {
         return
             currentPlayers.Any(p => Vector3.Distance(p.transform.position, pos) < minimumDistanceFromPlayer) ||
@@ -59,14 +56,24 @@ public class ParkingSpawnerController : MonoBehaviour
     }
     public void SpawnParking()
     {
+        int retriesCount = 0;
+        float currentMinimumDistanceFromParking = _minimumDistanceFromParking;
+
         // TODO: change it to work with object pooling
-        if (currentParkings.Count < 4)
+        if (currentParkings.Count < maxParkings)
         {
             Vector3 pos;
             do
             {
+                // lowers the distance allowed from parking if retries has reached the limit
+                if (retriesCount == spawnRetries && currentMinimumDistanceFromParking > 0)
+                {
+                    currentMinimumDistanceFromParking--; // Changing to /= 2 might be nicer
+                    retriesCount = 0;
+                }
                 pos = GetRandomPosition();
-            } while (IsPositionInvalid(pos));
+                retriesCount++;
+            } while (IsPositionInvalid(pos, _minimumDistanceFromPlayer, currentMinimumDistanceFromParking));
 
             var newParking = Instantiate(parking, pos, Quaternion.Euler(Vector3.up * Random.Range(0, 359)));
             currentParkings.Add(newParking.gameObject);
