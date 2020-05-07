be486817b7927a320b375bc0ee5b2046b9c1a19d
barshp <>
2020-05-07 03:01:03 +0300

    Added Photon PUN network menus and basic controller

---
+++ Assets/Scripts/Controllers/Network/CustomMatchmakingLobbyController.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomMatchmakingLobbyController : MonoBehaviourPunCallbacks
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Controllers/Network/NetworkController.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to: " + PhotonNetwork.CloudRegion);
+    }
+}
